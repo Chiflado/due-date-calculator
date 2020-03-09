10f3dacdffe0f753b7c828b9b7e1b8e40cb7f92f
Chiflado <prevarabalazs@gmail.com>
2020-03-09 19:20:02 +0100

    Adds test for due date with weekend

--- index.test.js
+++ index.test.js
@@ -14,15 +14,22 @@ test('calculate two day long due date', () => {
 
 test('sets end of the workday time', () => {
     const dayEnd = setDayEnd(1583764980000);
-    expect(dayEnd).toEqual(Date.parse('Mar 09 2020 17:00:00 GMT+0100'))
+    expect(dayEnd).toEqual(Date.parse('Mar 09 2020 17:00:00 GMT+0100'));
 })
 
 test('sets start of the next workday time', () => {
     const dayEnd = setNextDayStart(1583764980000);
-    expect(dayEnd).toEqual(Date.parse('Mar 10 2020 09:00:00 GMT+0100'))
+    expect(dayEnd).toEqual(Date.parse('Mar 10 2020 09:00:00 GMT+0100'));
 })
 
 test('calculate more than two day due date', () => {
     const dueDate = dueDateCalculator('Mar 09 2020 15:43:00 GMT+0100', oneHour * 10);
-    expect(dueDate).toEqual(new Date(1583916180000))
+    expect(dueDate).toEqual(new Date(1583916180000));
+})
+
+test('calculate due date with weekend', () => {
+    const dueDate = dueDateCalculator('Mar 13 2020 15:43:00 GMT+0100', oneHour * 10);
+    expect(dueDate).toEqual(new Date(1584434580000));
+    const dueDate2 = dueDateCalculator('Mar 13 2020 15:43:00 GMT+0100', oneHour * 2);
+    expect(dueDate2).toEqual(new Date(1584348180000));
 })
