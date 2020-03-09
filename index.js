7a045da9b4c34140985fe777878f1a11c0e89e6f
Chiflado <prevarabalazs@gmail.com>
2020-03-09 19:13:30 +0100

    Calculates more than two day long due date

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ exports.dueDateCalculator = (start, turnaround) => {
 }
 
 function watchWorkingHours(startTime, turnaround) {
-    const dayEnd = setDayEnd(startTime);
+    let dayEnd = setDayEnd(startTime);
     const nextDayStart = setNextDayStart(startTime);
     if (startTime + turnaround > dayEnd) {
         let remainTurnaround = turnaround - (dayEnd - startTime);
-        return new Date(nextDayStart + remainTurnaround);
+        const dueDate = new Date(nextDayStart + remainTurnaround);
+        dayEnd = setDayEnd(dueDate);
+        if (dueDate > dayEnd) {
+            return watchWorkingHours(nextDayStart, remainTurnaround);
+        }
+        return dueDate;
     }
     return new Date(startTime + turnaround);
 }
