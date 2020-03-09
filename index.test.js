e812cab3fb19927c6a8bc7e25c842025279f7789
Chiflado <prevarabalazs@gmail.com>
2020-03-09 17:37:06 +0100

    Fixes test expects and adds test for workday start and end calc

--- index.test.js
+++ index.test.js
@@ -1,11 +1,21 @@
-const { dueDateCalculator } = require('./index');
+const { dueDateCalculator, setNextDayStart, setDayEnd } = require('./index');
  
 test('calculate one hour', () => {
     const test = dueDateCalculator('Mar 09 2020 15:43:00 GMT+0100', 3600000);
-    expect(test).toBe(Date(1583768580000));
+    expect(test).toEqual(new Date(1583768580000));
 });
 
 test('calculate two day long due date', () => {
     const dueDate = dueDateCalculator('Mar 09 2020 15:43:00 GMT+0100', 7200000);
-    expect(dueDate).toBe(Date(1583829780000));
+    expect(dueDate).toEqual(new Date(1583829780000));
+})
+
+test('sets end of the workday time', () => {
+    const dayEnd = setDayEnd(1583764980000);
+    expect(dayEnd).toEqual(Date.parse('Mar 09 2020 17:00:00 GMT+0100'))
+})
+
+test('sets start of the next workday time', () => {
+    const dayEnd = setNextDayStart(1583764980000);
+    expect(dayEnd).toEqual(Date.parse('Mar 10 2020 09:00:00 GMT+0100'))
 })
