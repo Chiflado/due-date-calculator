78f73033aebf17935150a44a9f10b3db7098560a
Chiflado <prevarabalazs@gmail.com>
2020-03-09 19:37:50 +0100

    Adds test to set mondays if next day is weekend

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const { dueDateCalculator } = require('./index');
-const { setNextDayStart, setDayEnd } = require('./utils');
+const { setNextDayStart, setDayEnd, watchWeekends } = require('./utils');
 const oneHour = 60*60*1000;
 
 test('calculate one hour', () => {
@@ -18,8 +18,8 @@ test('sets end of the workday time', () => {
 })
 
 test('sets start of the next workday time', () => {
-    const dayEnd = setNextDayStart(1583764980000);
-    expect(dayEnd).toEqual(Date.parse('Mar 10 2020 09:00:00 GMT+0100'));
+    const dayStart = setNextDayStart(1583764980000);
+    expect(dayStart).toEqual(Date.parse('Mar 10 2020 09:00:00 GMT+0100'));
 })
 
 test('calculate more than two day due date', () => {
@@ -32,4 +32,11 @@ test('calculate due date with weekend', () => {
     expect(dueDate).toEqual(new Date(1584434580000));
     const dueDate2 = dueDateCalculator('Mar 13 2020 15:43:00 GMT+0100', oneHour * 2);
     expect(dueDate2).toEqual(new Date(1584348180000));
+})
+
+test('sets next day start if next day is weekend', () => {
+    const nextDayStart1 = watchWeekends(1584172800000);
+    expect(nextDayStart1).toEqual(1584345600000);
+    const nextDayStart2 = watchWeekends(1584259200000);
+    expect(nextDayStart2).toEqual(1584345600000);
 })
