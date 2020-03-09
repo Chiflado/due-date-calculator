f86d59b826845a47e45e1624e2124f06242950ba
Chiflado <prevarabalazs@gmail.com>
2020-03-09 17:37:27 +0100

    Caclulates start and end of the workdays

--- index.js
+++ index.js
@@ -2,6 +2,23 @@
 
 exports.dueDateCalculator = (start, turnaround) => {
     const startTime = Date.parse(start);
-    const finishDate = Date(startTime + turnaround);
+    const finishDate = new Date(startTime + turnaround);
     return finishDate;
+}
+function watchWorkingHours(startTime, turnaround) {
+    const dayEnd = setDayEnd(startTime);
+    const nextDayStart = setNextDayStart(startTime);
+}
+
+exports.setDayEnd = (startTime) => {
+    const startDate = new Date(startTime);
+    const dayEnd = startDate.setHours(17, 0, 0, 0);
+    return dayEnd;
+}
+
+exports.setNextDayStart = (startTime) => {
+    const startDate = new Date(startTime);
+    startDate.setHours(9, 0, 0, 0);
+    let nextDayStart = startDate.setDate(startDate.getDate() + 1);
+    return nextDayStart;
 }
